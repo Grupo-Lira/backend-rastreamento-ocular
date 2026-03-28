817af770df5f4fc079938beb790eb7128d206ebf
Giovana Albanês <albanesgiovana@gmail.com>
2026-03-28 16:49:38 -0300

    refactor: atualiza os handlers c as funções corretas do service

--- src/auth/handler/auth.handler.js
+++ src/auth/handler/auth.handler.js
@@ -1,11 +1,11 @@
 import loginUsuarioDto from "../../dto/request/loginUsuario.js";
 import registerUsuarioDto from "../../dto/request/registerUsuario.js";
-import { loginDoutor, registrarDoutor } from "../service/authService.js";
+import { login, registrarUser } from "../service/authService.js";
 
 const registerHandler = async (req, res) => {
   try {
     const dadosDto = registerUsuarioDto(req.body);
-    await registrarDoutor(dadosDto);
+    await registrarUser(dadosDto);
     return res.status(201).send();
   } catch (err) {
     return res.status(err.status || 500).json({ error: err.message });
@@ -14,8 +14,8 @@ const registerHandler = async (req, res) => {
 
 const loginHandler = async (req, res) => {
   try {
-    const credenciaisDto = loginUsuarioDto(req.body);
-    const resultado = await loginDoutor(credenciaisDto);
+    const credenciaisDto = loginUsuarioDto(req.body); 
+    const resultado = await login(credenciaisDto); 
     return res.status(200).json({ token: resultado.token });
   } catch (err) {
     return res.status(err.status || 500).json({ error: err.message });
